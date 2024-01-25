aa16a8ab0227a4fd3bd1a64265637e24fefa2368
Anshuman Komawar <anshumankomawar@gmail.com>
2024-01-25 15:52:18 -0800

    Added custom(hopsotch) titlebar implementations) for macos and windows

--- tauri/app/page.js
+++ tauri/app/page.js
@@ -1,9 +1,6 @@
-import { Button } from "@/components/ui/button"
-
 export default function Home() {
   return (
     <main className="flex h-full flex-col items-center justify-between p-4">
-      <Button>Button</Button>
     </main>
   );
 }
--- tauri/src-tauri/src/main.rs
+++ tauri/src-tauri/src/main.rs
@@ -1,8 +1,39 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+#[cfg(target_os = "macos")]
+#[macro_use]
+extern crate cocoa;
+
+#[cfg(target_os = "macos")]
+#[macro_use]
+extern crate objc;
+
+#[cfg(target_os = "macos")]
+mod mac;
+
+#[cfg(target_os = "windows")]
+mod win;
+
 fn main() {
   tauri::Builder::default()
+    .setup(|app| {
+        if cfg!(target_os = "macos") {
+            #[cfg(target_os = "macos")]
+            use mac::window::setup_mac_window;
+
+            #[cfg(target_os = "macos")]
+            setup_mac_window(app);
+        } else if cfg!(target_os = "windows") {
+            #[cfg(target_os = "windows")]
+            use win::window::setup_win_window;
+
+            #[cfg(target_os = "windows")]
+            setup_win_window(app);
+        }
+
+        Ok(())
+    })
     .run(tauri::generate_context!())
     .expect("error while running tauri application");
 }
