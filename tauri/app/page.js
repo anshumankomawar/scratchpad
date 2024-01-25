8addc007ff29dbc80481b43f1dd2d386e2ae6e02
Anshuman Komawar <anshumankomawar@gmail.com>
2024-01-25 14:23:47 -0800

    Added tauri boilerplate for desktop application, included shadcn, tailwind and using bun runtime

---
+++ tauri/app/page.js
@@ -0,0 +1,9 @@
+import { Button } from "@/components/ui/button"
+
+export default function Home() {
+  return (
+    <main className="flex h-full flex-col items-center justify-between p-4">
+      <Button>Button</Button>
+    </main>
+  );
+}
