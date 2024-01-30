c1780738155ef26f1821a5c4bf6dc5df64c3cd3a
sameera <sameerabali5@gmail.com>
2024-01-30 08:40:27 -0800

    added tab view for login and signup

--- tauri/app/page.js
+++ tauri/app/page.js
@@ -1,10 +1,20 @@
 import { LoginForm } from '@/components/login/login_form';
 import Tiptap from '../components/Tiptap'
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
+
 
 export default function Home() {
   return (
     <main className="flex h-full flex-col items-center justify-between p-4">
-      <LoginForm />
+      <Tabs defaultValue="login" className="w-[400px]">
+        <TabsList>
+          <TabsTrigger value="login">Login</TabsTrigger>
+          <TabsTrigger value="signup">Sign Up</TabsTrigger>
+        </TabsList>
+        <TabsContent value="login"><LoginForm/></TabsContent>
+        <TabsContent value="signup">Change your password here.</TabsContent>
+      </Tabs>
+
     </main>
   );
 }
