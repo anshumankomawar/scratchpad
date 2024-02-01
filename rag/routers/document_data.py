7acad00f70361f7c2f90c1ec5905b0d567dddb19
TariniS <tareene@gmail.com>
2024-02-01 14:21:55 -0800

    fixed get documents endpoint, gets documents for user based on auth credentials

--- rag/routers/document_data.py
+++ rag/routers/document_data.py
@@ -1,23 +1,18 @@
 from fastapi import APIRouter, Depends
 from typing import Annotated
 from dependencies import get_db
+from auth.oauth import get_current_user
+from models.user import User
 
-router = APIRouter(tags=["document_data"], dependencies=[Depends(get_db)])
+router = APIRouter(tags=["document_data"], dependencies=[Depends(get_db), Depends(get_current_user)])
 
-@router.get("/document_data", tags=["document_data"])
-def get_document_data(db: Annotated[dict, Depends(get_db)]):
+@router.get("/get_documents")
+async def get_documents(db: Annotated[dict, Depends(get_db)], current_user: Annotated[User, Depends(get_current_user)]):
     try:
-        documents = db["client"].from_("document_data").select("*").execute()
-        return {"message": documents}
-    except Exception as e:
-        print("Error", e)
-        return {"message": "couldnt get documents"}
-
-@router.get("/get_documents/{email}", tags=["document_data"])
-def get_document_data(db: Annotated[dict, Depends(get_db)], email:str):
-    try:
+        email = current_user["email"]
         documents = db["client"].from_("document_data").select("*").eq("email", email).execute()
         return {"message": documents}
     except Exception as e:
         print("Error", e)
         return {"message": "couldnt get documents for specified user"}
+    
