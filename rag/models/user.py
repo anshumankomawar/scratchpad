24804afc901dbdb6002b8f47c0acf352f03856f0
Anshuman Komawar <anshumankomawar@gmail.com>
2024-02-01 21:37:36 -0800

    Fixed user model bug with none type

--- rag/models/user.py
+++ rag/models/user.py
@@ -3,9 +3,9 @@ from pydantic import BaseModel
 
 class User(BaseModel):
     username: str
-    email: str | None = None
-    password: str | None = None
-    full_name: str | None = None
+    email: str 
+    password: str
+    full_name: str
 
 
 class UserPerm(BaseModel):
