d63563b058e44b8eb89466922c47c90cd448ed3b
Anshuman Komawar <anshumankomawar@gmail.com>
2024-02-05 13:25:50 -0800

    Removed api related queries from frontend

--- tauri-react/src/api_requests/auth_options.ts
+++
@@ -1,9 +0,0 @@
-import { fetchAuth } from './auth'
-import { queryOptions } from '@tanstack/react-query'
-
-export const authQueryOptions = (username: string, password: string) =>
-  queryOptions({
-    queryKey: ['login'],
-    queryFn: () => fetchAuth(username, password),
-    enabled: false
-  })
