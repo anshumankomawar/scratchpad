9840691cb6b8d994f93319be99369eae8098ddab
Anshuman Komawar <anshumankomawar@gmail.com>
2024-02-05 13:23:40 -0800

    Added get documents title, content for authenticated user

--- rag/routers/document_data.py
+++ rag/routers/document_data.py
@@ -1,6 +1,8 @@
 from fastapi import APIRouter, Depends
 from typing import Annotated
+from auth.oauth import get_current_user
 from dependencies import get_db
+from models.user import User
 
 router = APIRouter(tags=["document_data"], dependencies=[Depends(get_db)])
 
@@ -12,3 +14,13 @@ def get_document_data(db: Annotated[dict, Depends(get_db)]):
     except Exception as e:
         print("Error", e)
         return {"message": "couldnt get documents"}
+
+@router.get("/document")
+async def get_user_documents(db: Annotated[dict, Depends(get_db)], current_user: Annotated[User, Depends(get_current_user)]):
+    print(current_user)
+    try:
+        result = db["client"].from_('document_data').select('filename, content').eq('email', current_user["email"]).execute()
+        return {"documents": result.data}
+    except Exception as e:
+        print("Error", e)
+        return {"message": "couldnt get users documents"}
