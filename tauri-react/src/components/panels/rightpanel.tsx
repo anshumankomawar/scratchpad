929bc790f0f8cb76bc2aa02c5db0e004ff3bed92
Anshuman Komawar <anshumankomawar@gmail.com>
2024-02-07 19:45:17 -0800

    Fixed overflow scrolling for right panel

--- tauri-react/src/components/panels/rightpanel.tsx
+++ tauri-react/src/components/panels/rightpanel.tsx
@@ -7,7 +7,7 @@ type RightPanelProps = {
 
 export default function RightPanel({ isRightPanelOpen, children }: RightPanelProps) {
   return (
-    <div className={cn("border-l min-w-[150px] w-1/12 transition-all duration-250 ease-in-out overflow-hidden", { "min-w-0 w-0 md:w-0 lg:w-0 translate-x-full": !isRightPanelOpen })}>
+    <div className={cn("overflow-y-auto border-l min-w-[150px] w-1/12 transition-all duration-250 ease-in-out overflow-x-hidden", { "min-w-0 w-0 md:w-0 lg:w-0 translate-x-full": !isRightPanelOpen })}>
       <div className="p-2 h-full w-full">
         {children}
       </div>
