d29405ed192c6d0266cdd4f7acb228c2750d11ad
sameera <sameerabali5@gmail.com>
2024-02-10 13:38:02 -0800

    new UI added / changed login

--- tauri-react/src/components/login/login_form.tsx
+++ tauri-react/src/components/login/login_form.tsx
@@ -1,19 +1,5 @@
 import { Button } from "@/components/ui/button"
-import {
-  Card,
-  CardContent,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card"
 import { Input } from "@/components/ui/input"
-import { Label } from "@/components/ui/label"
-import {
-  Tabs,
-  TabsContent,
-  TabsList,
-  TabsTrigger,
-} from "@/components/ui/tabs"
 import { LoginComponentProps } from "./login"
 
 export function LoginForm(props: LoginComponentProps) {
@@ -22,61 +8,74 @@ export function LoginForm(props: LoginComponentProps) {
   };
 
   return (
-    <Tabs defaultValue="login" className="w-4/6">
-      <TabsList className="grid w-full grid-cols-2">
-        <TabsTrigger value="login">Login</TabsTrigger>
-        <TabsTrigger value="signup">Sign Up</TabsTrigger>
-      </TabsList>
-      <TabsContent value="login">
-        <Card>
-          <CardHeader>
-            <CardTitle>Login</CardTitle>
-          </CardHeader>
-          <CardContent className="space-y-2">
-            <div className="space-y-1">
-              <Label htmlFor="email">Email</Label>
-              <Input id="email" type="Email" placeholder="Email" value={props.email} 
-                onChange={(e) => props.setEmail(e.target.value)} required/>
-            </div>
-            <div className="space-y-1">
-              <Label htmlFor="password">Password</Label>
-              <Input id="password" type="Password" placeholder="Password" value={props.password} 
-                onChange={(e) => props.setPassword(e.target.value)} required/>
-            </div>
-          </CardContent>
-          <CardFooter>
-          <Button onClick={() => handleSubmit()}>
-              Login
-            </Button>
-          </CardFooter>
-        </Card>
-      </TabsContent>
-      <TabsContent value="signup">
-        <Card>
-          <CardHeader>
-            <CardTitle>Sign Up</CardTitle>
-          </CardHeader>
-          <CardContent className="space-y-2">
-            <div className="space-y-1">
-              <Label htmlFor="fullname">Full Name</Label>
-              <Input id="fullname" type="fullname" placeholder="Full Name"/>
-            </div>
-            <div className="space-y-1">
-              <Label htmlFor="email">Email</Label>
-              <Input id="email" type="email" placeholder="Email"/>
-            </div>
-            <div className="space-y-1">
-              <Label htmlFor="password">Password</Label>
-              <Input id="password" type="email" placeholder="Password" />
-            </div>
-          </CardContent>
-          <CardFooter>
-            <Button onClick={() => handleSubmit()}>
-              Signup
-            </Button>
-          </CardFooter>
-        </Card>
-      </TabsContent>
-    </Tabs>
+    <div className = "font-virgil">
+      <div className="space-y-4 w-full items-center">
+          <Input id="email" type="Email" placeholder="Email" value={props.email} 
+          onChange={(e) => props.setEmail(e.target.value)} required/>
+          <Input id="password" type="Password" placeholder="Password" value={props.password} 
+          onChange={(e) => props.setPassword(e.target.value)} required/>
+        <div className="w-full items-center justify-center flex pt-2"> 
+          <Button className="font-virgil w-[80px] h-[24px]" onClick={() => handleSubmit()}>Login</Button>
+        </div>
+      </div>
+
+    </div>
+
+    // <Tabs defaultValue="login" className="w-4/6">
+    //   <TabsList className="grid w-full grid-cols-2">
+    //     <TabsTrigger value="login">Login</TabsTrigger>
+    //     <TabsTrigger value="signup">Sign Up</TabsTrigger>
+    //   </TabsList>
+    //   <TabsContent value="login">
+    //     <Card>
+    //       <CardHeader>
+    //         <CardTitle className="font-virgil">Login</CardTitle>
+    //       </CardHeader>
+    //       <CardContent className="space-y-2">
+    //         <div className="space-y-1">
+    //           <Label className="font-virgil" htmlFor="email">Email</Label>
+    //           <Input id="email" type="Email" placeholder="Email" value={props.email} 
+    //             onChange={(e) => props.setEmail(e.target.value)} required/>
+    //         </div>
+    //         <div className="space-y-1">
+    //           <Label className="font-virgil" htmlFor="password">Password</Label>
+    //           <Input id="password" type="Password" placeholder="Password" value={props.password} 
+    //             onChange={(e) => props.setPassword(e.target.value)} required/>
+    //         </div>
+    //       </CardContent>
+    //       <CardFooter>
+    //       <Button className="font-virgil" onClick={() => handleSubmit()}>
+    //           Login
+    //         </Button>
+    //       </CardFooter>
+    //     </Card>
+    //   </TabsContent>
+    //   <TabsContent value="signup">
+    //     <Card>
+    //       <CardHeader>
+    //         <CardTitle className="font-virgil">Sign Up</CardTitle>
+    //       </CardHeader>
+    //       <CardContent className="space-y-2">
+    //         <div className="space-y-1">
+    //           <Label className="font-virgil" htmlFor="fullname">Full Name</Label>
+    //           <Input id="fullname" type="fullname" placeholder="Full Name"/>
+    //         </div>
+    //         <div className="space-y-1">
+    //           <Label className="font-virgil" htmlFor="email">Email</Label>
+    //           <Input id="email" type="email" placeholder="Email"/>
+    //         </div>
+    //         <div className="space-y-1">
+    //           <Label className="font-virgil" htmlFor="password">Password</Label>
+    //           <Input id="password" type="email" placeholder="Password" />
+    //         </div>
+    //       </CardContent>
+    //       <CardFooter>
+    //         <Button onClick={() => handleSubmit()}>
+    //           Signup
+    //         </Button>
+    //       </CardFooter>
+    //     </Card>
+    //   </TabsContent>
+    // </Tabs>
   )
 }
--- tauri-react/tailwind.config.js
+++ tauri-react/tailwind.config.js
@@ -7,6 +7,10 @@ module.exports = {
   ],
   prefix: "",
   theme: {
+    fontFamily: {
+      virgil: ["Virgil", "sans-serif"]
+
+    },
     container: {
       center: true,
       padding: "2rem",
