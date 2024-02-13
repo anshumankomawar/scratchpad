c2145ab90a55d8f59019ffa4a5a30380daaa2d82
Anshuman Komawar <anshumankomawar@gmail.com>
2024-02-12 19:36:38 -0800

    Added statusline

--- tauri-react/src/routes/index.tsx
+++ tauri-react/src/routes/index.tsx
@@ -9,6 +9,7 @@ import BottomFloatingPanel from '@/components/panels/bottomfloatingpanel';
 import { invoke } from "@tauri-apps/api/core"
 import { ToastAction } from "@/components/ui/toast"
 import { toast } from '@/components/ui/use-toast';
+import { Button } from '@/components/ui/button'
 
 interface Document {
   id: string,
@@ -20,6 +21,9 @@ interface LoaderData {
 }
 
 import RightFloatingPanel from '@/components/panels/rightfloatingpanel';
+import Save from '@/components/save/save';
+import { Popover, PopoverContent, PopoverTrigger } from '@/components/ui/popover';
+import { Moon, Sun } from 'lucide-react';
 
 export const Route = createFileRoute('/')({
   component: HomeComponent
@@ -107,6 +111,25 @@ function HomeComponent() {
         <BottomFloatingPanel open={openBottom} toggleBottomPanel={toggleBottomPanel} updateDocuments={updateDocuments} editor={tiptap.editor}/>
         <RightFloatingPanel open={openRight} toggleRightPanel={toggleRightPanel} editor={tiptap.editor}/>
         <EditorContent className="lg:mx-[250px] mx-[100px] overflow-x-hidden pt-4 no-scrollbar" editor={tiptap.editor} />
+        <div className="z-100 bottom-0 left-0 fixed h-min flex flex-row w-full font-virgil items-center px-5 py-4 bg-white">
+          <Popover>
+            <PopoverContent className='p-1 ml-2 mb-2 flex flex-col overflow-y-scroll space-y-1 w-32 font-virgil text-xs text-start'>
+              <Button variant="ghost" size="menu" className="text-xs justify-start p-2" onClick={toggleLeftPanel}>Open</Button>
+              <Button variant="ghost" size="menu" className="text-xs justify-start p-2" onClick={toggleBottomPanel}>Files</Button>
+              <Button variant="ghost" size="menu" className="text-xs justify-between p-2" onClick={toggleBottomPanel}>
+                Theme
+                <Sun className="ml-4 mr-1 hover:cursor-pointer" size={16} />
+              </Button>
+            </PopoverContent>
+            <PopoverTrigger className='text-start text-xs hover:cursor-pointer'>Settings</PopoverTrigger>
+          </Popover>
+          <div className="grow"></div>
+          <div className="text-xs">filename.txt</div>
+          <div className="grow"></div>
+          <div className="text-end text-xs">
+            {tiptap.editor.storage.characterCount.words()} | {tiptap.editor.storage.characterCount.characters()}
+          </div>
+        </div>
     </div>
   )
 }
