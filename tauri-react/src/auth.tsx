08accaa6a90a1df40e69587b7e8819b4ac613a4b
Anshuman Komawar <anshumankomawar@gmail.com>
2024-02-14 13:47:27 -0800

    Fixed formatting issue

--- tauri-react/src/auth.tsx
+++ tauri-react/src/auth.tsx
@@ -2,22 +2,22 @@ import * as React from "react";
 import { Store } from "@tauri-apps/plugin-store";
 
 export interface StoreContext {
-	store: Store;
+  store: Store;
 }
 
 const StoreContext = React.createContext<StoreContext | null>(null);
 
 export function StoreProvider({ children }: { children: React.ReactNode }) {
-	const store = new Store("config.json");
-	return (
-		<StoreContext.Provider value={{ store }}>{children}</StoreContext.Provider>
-	);
+  const store = new Store("config.json");
+  return (
+    <StoreContext.Provider value={{ store }}>{children}</StoreContext.Provider>
+  );
 }
 
 export function useStore() {
-	const context = React.useContext(StoreContext);
-	if (!context) {
-		throw new Error("useStore must be used within an StoreProvider");
-	}
-	return context;
+  const context = React.useContext(StoreContext);
+  if (!context) {
+    throw new Error("useStore must be used within an StoreProvider");
+  }
+  return context;
 }
