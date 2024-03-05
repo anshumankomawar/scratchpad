a9a1050f7612440f5da158d6ca122d1439477de0
sameera <sameerabali5@gmail.com>
2024-03-05 10:16:19 -0800

    validation for accounts

--- tauri-react/src/components/account/account.tsx
+++ tauri-react/src/components/account/account.tsx
@@ -20,155 +20,91 @@ import {
 	FormLabel,
 	FormMessage,
 } from "@/components/ui/form";
+
+import { Input } from "@/components/ui/input"
+
 import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group";
 import { Separator } from "@radix-ui/react-separator";
 import { useFontFamily } from "@/context/font_context";
 import { useTheme } from "@/context/theme_context";
 
-const appearanceFormSchema = z.object({
-	theme: z.enum(["light", "dark"], {
-		required_error: "Please select a theme.",
-	}),
-	font: z.enum(["Virgil", "Barlow"], {
-		invalid_type_error: "Select a font",
-		required_error: "Please select a font.",
-	}),
-});
 
-type AppearanceFormValues = z.infer<typeof appearanceFormSchema>;
+const formSchema = z.object({
+	username: z.string().min(2, {
+	  message: "Username must be at least 2 characters.",
+	}),
+	password: z.string().min(8, {
+		message: "Password must be at least 8 characters."
+	}),
+	email: z.string()
+    .min(1, { message: "This field has to be filled." })
+    .email("This is not a valid email.")
+  })
 
-// This can come from your database or API.
 
 export default function AccountPage() {
-	const { fontFamily, setFontFamily } = useFontFamily();
-	const { setTheme } = useTheme();
-
-	const defaultValues: Partial<AppearanceFormValues> = {
-		theme: "light",
-		font: fontFamily,
-	};
-
-	const form = useForm<AppearanceFormValues>({
-		resolver: zodResolver(appearanceFormSchema),
-		defaultValues,
-	});
-
-	function onSubmit(data: AppearanceFormValues) {
-		console.log(data);
-		setFontFamily(data.font);
-		setTheme(data.theme);
+	const form = useForm<z.infer<typeof formSchema>>({
+		resolver: zodResolver(formSchema),
+		defaultValues: {
+		  username: "",
+		},
+	  })
+	
+	function onSubmit(values: z.infer<typeof formSchema>) {
+		// Do something with the form values.
+		console.log(values)
 	}
 
 	return (
 		<div className="flex flex-col overflow-y-hidden">
-			<div className="text-bold text-lg pb-1">Settings</div>
+			<div className="text-bold text-lg pb-1">Account Details</div>
 			<div className="text-sm text-gray-400">
-				Mange your account preferences.
+				Mange your ScratchPad profile
 			</div>
 			<Separator orientation="horizontal" />
-			<div className="overflow-y-auto pt-8">
+			<div className="pt-10">
 				<Form {...form}>
 					<form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-						<FormField
-							control={form.control}
-							name="font"
-							render={({ field }) => (
-								<FormItem>
-									<FormLabel>Font</FormLabel>
-									<FormControl defaultValue={fontFamily}>
-										<Select
-											defaultValue={fontFamily}
-											onValueChange={field.onChange}
-										>
-											<SelectTrigger className="w-[180px]" {...field}>
-												<SelectValue placeholder="Theme" />
-											</SelectTrigger>
-											<SelectContent>
-												<SelectItem value="Virgil">Virgil</SelectItem>
-												<SelectItem value="Barlow">Barlow</SelectItem>
-											</SelectContent>
-										</Select>
-									</FormControl>
-									<ChevronDownIcon className="absolute right-3 top-2.5 h-4 w-4 opacity-50" />
-									<FormDescription>
-										Set the font you want to use in the dashboard.
-									</FormDescription>
-									<FormMessage />
-								</FormItem>
-							)}
-						/>
-						<FormField
-							control={form.control}
-							name="theme"
-							render={({ field }) => (
-								<FormItem className="space-y-1">
-									<FormLabel>Theme</FormLabel>
-									<FormDescription>
-										Select the theme for the dashboard.
-									</FormDescription>
-									<FormMessage />
-									<RadioGroup
-										onValueChange={field.onChange}
-										defaultValue={field.value}
-										className="grid max-w-md grid-cols-2 gap-8 pt-2"
-									>
-										<FormItem>
-											<FormLabel className="[&:has([data-state=checked])>div]:border-primary">
-												<FormControl>
-													<RadioGroupItem value="light" className="sr-only" />
-												</FormControl>
-												<div className="items-center rounded-md border-2 border-muted p-1 hover:border-accent">
-													<div className="space-y-2 rounded-sm bg-[#ecedef] p-2">
-														<div className="space-y-2 rounded-md bg-white p-2 shadow-sm">
-															<div className="h-2 w-[80px] rounded-lg bg-[#ecedef]" />
-															<div className="h-2 w-[100px] rounded-lg bg-[#ecedef]" />
-														</div>
-														<div className="flex items-center space-x-2 rounded-md bg-white p-2 shadow-sm">
-															<div className="h-4 w-4 rounded-full bg-[#ecedef]" />
-															<div className="h-2 w-[100px] rounded-lg bg-[#ecedef]" />
-														</div>
-														<div className="flex items-center space-x-2 rounded-md bg-white p-2 shadow-sm">
-															<div className="h-4 w-4 rounded-full bg-[#ecedef]" />
-															<div className="h-2 w-[100px] rounded-lg bg-[#ecedef]" />
-														</div>
-													</div>
-												</div>
-												<span className="block w-full p-2 text-center font-normal">
-													Light
-												</span>
-											</FormLabel>
-										</FormItem>
-										<FormItem>
-											<FormLabel className="[&:has([data-state=checked])>div]:border-primary">
-												<FormControl>
-													<RadioGroupItem value="dark" className="sr-only" />
-												</FormControl>
-												<div className="items-center rounded-md border-2 border-muted bg-popover p-1 hover:bg-accent hover:text-accent-foreground">
-													<div className="space-y-2 rounded-sm bg-slate-950 p-2">
-														<div className="space-y-2 rounded-md bg-slate-800 p-2 shadow-sm">
-															<div className="h-2 w-[80px] rounded-lg bg-slate-400" />
-															<div className="h-2 w-[100px] rounded-lg bg-slate-400" />
-														</div>
-														<div className="flex items-center space-x-2 rounded-md bg-slate-800 p-2 shadow-sm">
-															<div className="h-4 w-4 rounded-full bg-slate-400" />
-															<div className="h-2 w-[100px] rounded-lg bg-slate-400" />
-														</div>
-														<div className="flex items-center space-x-2 rounded-md bg-slate-800 p-2 shadow-sm">
-															<div className="h-4 w-4 rounded-full bg-slate-400" />
-															<div className="h-2 w-[100px] rounded-lg bg-slate-400" />
-														</div>
-													</div>
-												</div>
-												<span className="block w-full p-2 text-center font-normal">
-													Dark
-												</span>
-											</FormLabel>
-										</FormItem>
-									</RadioGroup>
-								</FormItem>
-							)}
-						/>
-						<Button type="submit">Update preferences</Button>
+					<FormField
+						control={form.control}
+						name="username"
+						render={({ field }) => (
+							<FormItem>
+							<FormLabel>Username</FormLabel>
+							<FormControl>
+								<Input placeholder="john doe" {...field} />
+							</FormControl>
+							<FormMessage />
+							</FormItem>
+						)}
+					/>
+					<FormField
+						control={form.control}
+						name="password"
+						render={({ field }) => (
+							<FormItem>
+							<FormLabel>Password</FormLabel>
+							<FormControl>
+								<Input placeholder="*****" {...field} />
+							</FormControl>
+							<FormMessage />
+							</FormItem>
+						)}
+					/>
+					<FormField
+						control={form.control}
+						name="email"
+						render={({ field }) => (
+							<FormItem>
+							<FormLabel>Email</FormLabel>
+							<FormControl>
+								<Input placeholder="john@gmail.com" {...field} />
+							</FormControl>
+							<FormMessage />
+							</FormItem>
+						)}
+					/>
+					<Button classname="align-bottom" type="submit">Update preferences</Button>
 					</form>
 				</Form>
 			</div>
