8cfc45d0cf4cf982cc8cdecbd4c85aa2a7ebcbad
TariniS <tareene@gmail.com>
2024-03-11 19:13:23 -0700

    added delete folders, make folders inactive

--- rag/routers/folders.py
+++ rag/routers/folders.py
@@ -150,3 +150,22 @@ def get_or_make_generated_folder(
         print("Error", e)
         return {"message": "Could not find generated directory"}
 
+# Delete folder
+@router.delete("/folders")
+def delete_folder(db: Annotated[dict, Depends(get_db)], current_user: Annotated[User, Depends(get_current_user)], id:str):
+    try:
+        email = current_user["email"]
+        # Check if the folder exists and belongs to the current user
+        folder = db["client"].from_("folders").select("*").eq("email",email).eq("id", id).execute()
+        if folder:
+            # Delete folder
+            # isActive to false
+            db["client"].from_("folders").update({'isActive':False}).eq("email", email).eq("id", id).execute()
+            # setting each document inside this folder to not active
+            db["client"].from_("documents").update({'isActive': False}).eq("email", email).eq("folder_id", id).execute()
+        else:
+            raise HTTPException(404, detail="Folder not found")
+        return {"message": "Folder deleted successfully"}
+    except Exception as e:
+        print("Error", e)
+        return {"message": "could not delete folder"}
